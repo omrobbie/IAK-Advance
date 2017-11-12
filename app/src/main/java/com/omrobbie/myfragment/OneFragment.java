9031517c47cccede3fe0ca27faf6b676f27b1233
omrobbie <putu.roby@yahoo.com>
2017-11-12 10:27:36 +0800

    connect fragment one to container on main activity

---
+++ app/src/main/java/com/omrobbie/myfragment/OneFragment.java
@@ -0,0 +1,22 @@
+package com.omrobbie.myfragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by omrobbie on 12/11/2017.
+ */
+
+public class OneFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_one, container, false);
+        return view;
+    }
+}
